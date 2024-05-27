593842da739c0a5b533cfca530a292ed51e66e23
inseokLee999 <syt06137@naver.com>
2024-05-27 16:09:28 +0900

    Setting

---
+++ src/main/java/org/choongang/main/controllers/RequestJoin.java
@@ -0,0 +1,13 @@
+package org.choongang.main.controllers;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RequestJoin {
+    private String userId;
+    private String userPw;
+    private String confirmPw;
+    private String userNm;
+}
--- src/main/java/org/choongang/member/controllers/JoinController.java
+++ src/main/java/org/choongang/member/controllers/JoinController.java
@@ -2,7 +2,11 @@ package org.choongang.member.controllers;
 
 import org.choongang.global.AbstractController;
 import org.choongang.global.constants.Menu;
+import org.choongang.main.MainRouter;
+import org.choongang.main.controllers.RequestJoin;
 import org.choongang.template.Templates;
+
+import java.util.function.Predicate;
 
 /**
  * 회원 가입 컨트롤러
@@ -11,5 +15,27 @@ public class JoinController extends AbstractController {
     @Override
     public void show() {
         Templates.getInstance().render(Menu.JOIN);
+    }
+
+    @Override
+    public void prompt() {
+        String userId = promptWithValidation("아이디(6자리 이상) : ",s -> s.length()>=6);
+        String userPw = promptWithValidation("비밀번호(8자리 이상) : ",s -> s.length()>=8);
+        String confirmPw = promptWithValidation("비밀번호 확인 : ",s -> {
+            boolean match = s.equals(userPw);
+            if(!match)System.err.println("\n비밀번호가 일치하지 않습니다.");
+            return match;
+        });
+        String userNm = promptWithValidation("사용자 이름:",s -> !s.isBlank());
+        RequestJoin form = RequestJoin.builder()
+                        .userId(userId)
+                        .userPw(userPw)
+                        .confirmPw(confirmPw)
+                        .userNm(userNm)
+                        .build();
+        //회원가입 처리
+        System.out.println(form);
+        //회원가입 성공시 -> 로그인화면 이동
+        MainRouter.getInstance().change(Menu.LOGIN);
     }
 }
