a018587581f1204f928a632c5eea7f192c794f5d
yoojung275 <yoojung275@gmail.com>
2024-05-31 15:34:44 +0900

    AbstractControllerLocator 생성, 로그인 페이지에서 서브메뉴 1. 학생로그인/ 2. 관리자 로그인으로 나뉘도록 수정

--- src/main/java/org/choongang/member/controllers/RequestLogin.java
+++ src/main/java/org/choongang/member/controllers/RequestLogin.java
@@ -2,9 +2,11 @@ package org.choongang.member.controllers;
 
 import lombok.Builder;
 import lombok.Data;
+import org.choongang.member.constants.UserType;
 
 @Data @Builder
 public class RequestLogin {
     private String userId;
     private String userPw;
+    private UserType userType;
 }
