89bfff8f6f2cc1002bc8b62f98006bb3fba6baa6
jangjangji <sjjang41@naver.com>
2024-06-02 17:19:10 +0900

    Attend Mybatis 기본 형태 구현

---
+++ src/main/java/org/choongang/attend/entities/Attend.java
@@ -0,0 +1,21 @@
+package org.choongang.attend.entities;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Attend {
+    private long attendCode;
+    private String sutdentName;
+    private String department;
+    private String studentId;
+    private String oneWeek;
+    private String twoWeek;
+    private String threeWeek;
+    private String fourWeek;
+    private String fiveWeek;
+    private String sixWeek;
+    private String sevenWeek;
+
+}
---
+++ src/main/java/org/choongang/attend/mapper/AttendMapper.java
@@ -0,0 +1,4 @@
+package org.choongang.attend.mapper;
+
+public interface AttendMapper {
+}
