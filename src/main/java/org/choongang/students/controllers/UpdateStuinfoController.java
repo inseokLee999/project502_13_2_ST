167f86387b6b72d5efe328ccdb912d7e66b1d50f
irine3355 <irine3355@gmail.com>
2024-06-03 20:59:09 +0900

    학생정보페이지 연결

--- src/main/java/org/choongang/students/controllers/UpdateStuinfoController.java
+++ src/main/java/org/choongang/students/controllers/UpdateStuinfoController.java
@@ -22,7 +22,7 @@ public class UpdateStuinfoController extends AbstractController {
             while (true) {
                 String userNoStr = promptWithValidation("수정할 학생의 학번을 입력하세요 (종료 입력 시 종료) : ", s -> {
                     if (s.equals("종료")) {
-                        router.change(SubMenu.STUINFO);
+                        router.change(SubMenu.PRIVACY);
                         return false;
                     }
                     return !s.isBlank();
