6fdcf1d0c8dd3904f56104a6334259314f8dac40
irine3355 <irine3355@gmail.com>
2024-06-03 12:42:34 +0900

    관리자모드 페이지

---
+++ src/main/java/org/choongang/template/after_login_admin_menu/Lecture_Info_Mngm_Tpl.java
@@ -0,0 +1,14 @@
+package org.choongang.template.after_login_admin_menu;
+
+import org.choongang.template.Template;
+
+public class Lecture_Info_Mngm_Tpl implements Template {
+
+    @Override
+    public String getTpl() {
+        StringBuffer sb = new StringBuffer(1000);
+        sb.append("강의 정보 관리입니다.\n");
+        sb.append("이전화면으로 돌아가려면 1번을 눌러주세요");
+        return sb.toString();
+    }
+}
