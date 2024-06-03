ea05b1fd10ce7ade75c14a3229d2fa8097fa8a5f
jangjangji <sjjang41@naver.com>
2024-06-03 11:11:22 +0900

    출석화면 변경 1. 아이디 값 입력한거 받아오기 구현 2. 입력한 아이디 값에 따라 출석현황을 사용자에 맞는 데이터 출력 완료

---
+++ src/main/java/org/choongang/member/UserSession.java
@@ -0,0 +1,24 @@
+package org.choongang.member;
+
+public class UserSession {
+    private static UserSession instance;
+    private String userId;
+
+    private UserSession() {
+    }
+
+    public static UserSession getInstance() {
+        if (instance == null) {
+            instance = new UserSession();
+        }
+        return instance;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
